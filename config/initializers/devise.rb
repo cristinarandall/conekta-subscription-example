a420a45c340991976b055353e56b6b1e4960c9aa
Cristina <cristina@conekta.mx>
2013-12-01 18:31:32 -0600

    mes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,6 +5,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
+config.secret_key = 'f1c635a78b289944cc4ca814fd27186ccf366d25bcbaaf8320f9e190604daf8ed6c4baf9c838040db9ee62520f2f95bea2355f2b54a41e8b71b5f28a4e0d23ce'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+
+# Be sure to restart your server when you modify this file.  
+  
+# Your secret key for verifying the integrity of signed cookies.  
+# If you change this key, all old signed cookies will become invalid!  
+# Make sure the secret is at least 30 characters and all random,   
+# no regular words or you'll be exposed to dictionary attacks.  
+Rails.application.config.cookie_secret = "e6549becc330029388391e82d9b1343772fe02e44246534b6589eff1496cb7ef3c86ba7ec960e943f189d9b013c006ce24503bf8e674fb5fe75c089c05286bb9"
+
+# "#{ActiveSupport::SecureRandom.hex(64)}"
+
+
